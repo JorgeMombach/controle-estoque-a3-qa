5069182280ceda4db274aa6fc365b968fd70592c
JamaykonCZ <jorge.mombach@gmail.com>
2024-11-01 18:01:29 -0300

    A3-002 [Adiciona EntradaProdutoService]

---
+++ src/main/java/com/controle_estoque/service/EntradaProdutoService.java
@@ -0,0 +1,51 @@
+package com.controle_estoque.service;
+
+import com.controle_estoque.entity.EntradaProduto;
+import com.controle_estoque.entity.Produto;
+import com.controle_estoque.repository.EntradaProdutoRepository;
+import com.controle_estoque.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EntradaProdutoService {
+
+    private final EntradaProdutoRepository entradaProdutoRepository;
+    private final ProdutoRepository produtoRepository;
+
+    @Autowired
+    public EntradaProdutoService(EntradaProdutoRepository entradaProdutoRepository, ProdutoRepository produtoRepository) {
+        this.entradaProdutoRepository = entradaProdutoRepository;
+        this.produtoRepository = produtoRepository;
+    }
+
+    public List<EntradaProduto> listarEntradas() {
+        return entradaProdutoRepository.findAll();
+    }
+
+    public EntradaProduto registrarEntrada(EntradaProduto entradaProduto) {
+        Optional<Produto> produtoOptional = produtoRepository.findById(entradaProduto.getProduto().getId());
+        if (produtoOptional.isPresent()) {
+            Produto produto = produtoOptional.get();
+            produto.setQuantidadeEstoque(produto.getQuantidadeEstoque() + entradaProduto.getQuantidade());
+            produtoRepository.save(produto);
+            return entradaProdutoRepository.save(entradaProduto);
+        } else {
+            throw new RuntimeException("Produto não encontrado com o ID: " + entradaProduto.getProduto().getId());
+        }
+    }
+
+    public Optional<EntradaProduto> encontrarEntradaPorId(Long id) {
+        return entradaProdutoRepository.findById(id);
+    }
+
+    public void excluirEntrada(Long id) {
+        if (!entradaProdutoRepository.existsById(id)) {
+            throw new RuntimeException("Entrada de produto não encontrada com o ID: " + id);
+        }
+        entradaProdutoRepository.deleteById(id);
+    }
+}

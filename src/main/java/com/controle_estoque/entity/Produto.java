100de67d9d8034bdda2806127ff1a958ef9b98ef
JamaykonCZ <jorge.mombach@gmail.com>
2024-11-01 16:39:12 -0300

    A3-001 [Refatora Produto e adiciona novos atributos]

--- src/main/java/com/controle_estoque/entity/Produto.java
+++ src/main/java/com/controle_estoque/entity/Produto.java
@@ -2,37 +2,81 @@ package com.controle_estoque.entity;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+
 @Entity
-@Table(name = "en_produto")
 public class Produto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idProduto;
-    private String nomeProduto;
-    private String descricaoProduto;
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String nome;
+    @Column(nullable = false)
+    private String descricao;
+    @Column(nullable = false)
+    private Integer quantidadeMinima;
+    @Column(nullable = false)
+    private Integer quantidadeMaxima;
+    @Column(nullable = false)
+    private LocalDateTime dataCriacao;
 
     public Produto() {
     }
 
-    public Produto(Long idProduto, String nomeProduto, String descricaoProduto) {
-        this.idProduto = idProduto;
-        this.nomeProduto = nomeProduto;
-        this.descricaoProduto = descricaoProduto;
+    public Produto(String nome, String descricao, Integer quantidadeMinima, Integer quantidadeMaxima, LocalDateTime dataCriacao) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.quantidadeMinima = quantidadeMinima;
+        this.quantidadeMaxima = quantidadeMaxima;
+        this.dataCriacao = dataCriacao;
     }
 
-    public Long getIdProduto() { return idProduto; }
-    public void setIdProduto(Long idProduto) {
-        this.idProduto = idProduto;
+    public Long getId() {
+        return id;
     }
 
-    public String getNomeProduto() { return nomeProduto; }
-    public void setNomeProduto(String nomeProduto) {
-        this.nomeProduto = nomeProduto;
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public String getDescricaoProduto() { return descricaoProduto; }
-    public void setDescricaoProduto(String descricaoProduto) {
-        this.descricaoProduto = descricaoProduto;
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getQuantidadeMinima() {
+        return quantidadeMinima;
+    }
+
+    public void setQuantidadeMinima(Integer quantidadeMinima) {
+        this.quantidadeMinima = quantidadeMinima;
+    }
+
+    public Integer getQuantidadeMaxima() {
+        return quantidadeMaxima;
+    }
+
+    public void setQuantidadeMaxima(Integer quantidadeMaxima) {
+        this.quantidadeMaxima = quantidadeMaxima;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
     }
 }

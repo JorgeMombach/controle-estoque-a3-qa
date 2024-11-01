1ee4688734bf86aed6a75438e7893fe976fa2bcf
JamaykonCZ <jorge.mombach@gmail.com>
2024-11-01 19:14:08 -0300

    A3-003 [Refatora respositorios para query fora do padrao para busca de elementos]

--- src/main/java/com/controle_estoque/repository/EntradaProdutoRepository.java
+++ src/main/java/com/controle_estoque/repository/EntradaProdutoRepository.java
@@ -1,9 +1,14 @@
 package com.controle_estoque.repository;
 
 import com.controle_estoque.entity.EntradaProduto;
+import com.controle_estoque.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EntradaProdutoRepository extends JpaRepository <EntradaProduto, Long>{
+
+    List<EntradaProduto> findByAtivoTrue();
 }

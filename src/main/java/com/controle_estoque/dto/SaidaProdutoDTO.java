958de81ed61a1968ba133a877f1c54fdf9449a80
JamaykonCZ <jorge.mombach@gmail.com>
2024-11-01 23:24:16 -0300

    A3-004 [Adiciona dto de saida de produto]

---
+++ src/main/java/com/controle_estoque/dto/SaidaProdutoDTO.java
@@ -0,0 +1,34 @@
+package com.controle_estoque.dto;
+
+import java.time.LocalDateTime;
+
+public class SaidaProdutoDTO {
+    private Long produtoId;
+    private int quantidade;
+    private LocalDateTime dataSaida;
+
+    // Getters e Setters
+    public Long getProdutoId() {
+        return produtoId;
+    }
+
+    public void setProdutoId(Long produtoId) {
+        this.produtoId = produtoId;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public LocalDateTime getDataSaida() {
+        return dataSaida;
+    }
+
+    public void setDataSaida(LocalDateTime dataSaida) {
+        this.dataSaida = dataSaida;
+    }
+}

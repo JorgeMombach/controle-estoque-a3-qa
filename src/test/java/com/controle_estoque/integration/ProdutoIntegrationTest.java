1cbb75c12b49d00b1b1a2bbc3fa651e032d39529
JeffCarradore <jeffersoncarradore001@gmail.com>
2024-11-26 16:52:20 -0300

    Adiciona teste de integração para salvar produto no banco de dados.

--- src/test/java/com/controle_estoque/integration/ProdutoIntegrationTest.java
+++ src/test/java/com/controle_estoque/integration/ProdutoIntegrationTest.java
@@ -1,8 +1,29 @@
 package com.controle_estoque.integration;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.controle_estoque.entity.Produto;
+import com.controle_estoque.repository.ProdutoRepository;
 
 @DataJpaTest
 class ProdutoIntegrationTest {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @Test
+    void testSalvarProduto() {
+        Produto produto = new Produto("Produto Teste", "Descrição Teste", 10.0, 100, true);
+
+        Produto produtoSalvo = produtoRepository.save(produto);
+
+        assertNotNull(produtoSalvo.getId());
+        assertEquals("Produto Teste", produtoSalvo.getNome());
+    }
 
 }

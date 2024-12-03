2187976b48aa1b888a22e8afde688c3c52c64e4d
JeffCarradore <jeffersoncarradore001@gmail.com>
2024-12-03 09:31:53 -0300

    Cria teste de GET(Lista).

--- src/test/java/com/controle_estoque/integration/ProdutoResourceIntegrationTest.java
+++ src/test/java/com/controle_estoque/integration/ProdutoResourceIntegrationTest.java
@@ -1,5 +1,7 @@
 package com.controle_estoque.integration;
 
+import com.controle_estoque.entity.Produto;
+import com.controle_estoque.repository.ProdutoRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,6 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -17,17 +20,20 @@ class ProdutoResourceIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
     @Test
     void testPostProduto() throws Exception {
         String produtoJson = """
-            {
-                "nome": "Produto Teste",
-                "descricao": "Descrição Teste",
-                "preco": 15.0,
-                "quantidadeEstoque": 100,
-                "ativo": true
-            }
-        """;
+                    {
+                        "nome": "Produto Teste",
+                        "descricao": "Descrição Teste",
+                        "preco": 15.0,
+                        "quantidadeEstoque": 100,
+                        "ativo": true
+                    }
+                """;
 
         mockMvc.perform(post("/api/produtos")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -36,9 +42,20 @@ class ProdutoResourceIntegrationTest {
                 .andExpect(jsonPath("$.id").exists())
                 .andExpect(jsonPath("$.nome").value("Produto Teste"))
                 .andExpect(jsonPath("$.descricao").value("Descrição Teste"))
-                .andExpect(jsonPath("$.preco").value(15.0))
+                .andExpect(jsonPath("$.preco").value(10.0))
                 .andExpect(jsonPath("$.quantidadeEstoque").value(100))
                 .andExpect(jsonPath("$.ativo").value(true));
+    }
+
+    @Test
+    void testListProduto() throws Exception {
+        Produto produto = new Produto("Produto Teste", "Descrição Teste", 10.0, 100, true);
+        produtoRepository.save(produto);
+
+        mockMvc.perform(get("/api/produtos"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(greaterThan(0))))
+                .andExpect(jsonPath("$[0].nome", is("Produto Teste")));
     }
 
 }
